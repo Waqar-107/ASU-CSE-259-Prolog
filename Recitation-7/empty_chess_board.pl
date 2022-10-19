071835072dd50babc2e3094fe98e3160c4716cc6
Waqar-107 <waqar.hassan866@gmail.com>
2022-10-19 02:33:49 -0700

    updated empty chess board

--- Recitation-7/empty_chess_board.pl
+++ Recitation-7/empty_chess_board.pl
@@ -1,30 +1,39 @@
 drawSymbol(Symbol, 0).
 drawSymbol(Symbol, N) :- N > 0, write(Symbol), N1 is N - 1, drawSymbol(Symbol, N1).
 
-main :- drawBoard(8).
 
-drawBoard(0) :- 
-  ColumnNumber is 8,
-  drawContentLine(ColumnNumber).
-  
-drawBoard(RowNumber) :-
-  RowNumber > 0,
-  ColumnNumber is 8,
-  drawContentLine(ColumnNumber),
-  drawContentCell(ColumnNumber),
-  NewRowNumber is RowNumber - 1,
-  drawBoard(NewRowNumber).  
+main :- drawBoard(8, 8).
 
-drawContentLine(0) :- write('+'), nl.
-drawContentLine(ColumnNumber) :-
-  ColumnNumber > 0,
-  write('+'), drawSymbol('-', 4),
-  NewColumnNumber is ColumnNumber - 1,
-  drawContentLine(NewColumnNumber).
 
-drawContentCell(0) :- write('|'), nl.
-drawContentCell(ColumnNumber) :-
-  ColumnNumber > 0,
-  write('|'), drawSymbol(' ', 4),
-  NewColumnNumber is ColumnNumber - 1,
-  drawContentCell(NewColumnNumber).
+drawBoard(0, Col) :- drawSymbol(' ', 1), drawBorderLine(Col), drawPair.
+drawBoard(Row, Col) :-
+  Row > 0,
+  drawSymbol(' ', 1),
+  drawBorderLine(Col),
+  drawSymbol(Row, 1),
+  drawContentCell(Row, Col),
+  NewRow is Row - 1,
+  drawBoard(NewRow, Col).
+
+
+drawBorderLine(0) :- drawSymbol('+', 1), nl.
+drawBorderLine(Col) :-
+  Col > 0,
+  drawSymbol('+', 1), drawSymbol('-', 4),
+  NewCol is Col - 1,
+  drawBorderLine(NewCol).
+
+
+drawContentCell(Row, 0) :- drawSymbol('|', 1), nl.
+drawContentCell(Row, Col) :-
+  Col > 0,
+  drawSymbol('|', 1), drawSymbol(' ', 4),
+  NewCol is Col - 1,
+  drawContentCell(Row, NewCol).
+
+
+drawPair :-
+  drawSymbol(' ', 4), drawSymbol('a', 1), drawSymbol(' ', 4), drawSymbol('b', 1),
+  drawSymbol(' ', 4), drawSymbol('c', 1), drawSymbol(' ', 4), drawSymbol('d', 1),
+  drawSymbol(' ', 4), drawSymbol('e', 1), drawSymbol(' ', 4), drawSymbol('f', 1),
+  drawSymbol(' ', 4), drawSymbol('g', 1), drawSymbol(' ', 4), drawSymbol('h', 1).
