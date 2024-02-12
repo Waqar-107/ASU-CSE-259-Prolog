e2054372a61ffe470c285e6403e91097047bbbb8
Waqar-107 <waqar.hassan866@gmail.com>
2024-02-12 13:55:04 -0700

    added template for project 1

---
+++ Project-templates/Printing ASU/printing_asu.pl
@@ -0,0 +1,124 @@
+drawSymbol(Symbol, 0).
+drawSymbol(Symbol, N) :- N > 0, write(Symbol), N1 is N - 1, drawSymbol(Symbol, N1).
+
+
+drawHorizontalLine(Symbol, 0) :- nl.
+drawHorizontalLine(Symbol, N) :- drawSymbol(Symbol, N).
+
+
+drawVerticalLinesWithSpace(Symbol, 0, Width).
+drawVerticalLinesWithSpace(Symbol, Height, Width) :- 
+  Height > 0,
+  write(Symbol),
+  drawSymbol(' ', Width - 2),
+  write(Symbol),
+  Height1 is Height - 1,
+  drawVerticalLinesWithSpace(Symbol, Height1, Width).
+
+
+/*-------------------------------------------------------------------------------------------------*/
+/* draw A */
+drawA(TextWidth, TextHeight, FontSize, CurrentLine, ColumnNumber) :-
+  ColumnNumber >= TextWidth.
+
+/* 
+ * Covers the left-most and the right-most columns that only have stars 
+ */
+drawA(TextWidth, TextHeight, FontSize, CurrentLine, ColumnNumber) :-
+  (
+    (ColumnNumber >= 0, ColumnNumber < FontSize);
+    (ColumnNumber >= FontSize * 2, ColumnNumber < TextWidth )
+  ),
+  drawSymbol('*', FontSize),
+  NextColumn is ColumnNumber + FontSize,
+  drawA(TextWidth, TextHeight, FontSize, CurrentLine, NextColumn).
+
+
+/* 
+ * Covers the middle segment
+ * Will have either stars or spaces 
+ */
+drawA(TextWidth, TextHeight, FontSize, CurrentLine, ColumnNumber) :-
+  (ColumnNumber >= FontSize, ColumnNumber < FontSize * 2),
+  (
+    (CurrentLine >= 0, CurrentLine < FontSize);
+    (CurrentLine >= FontSize * 2 , CurrentLine < FontSize * 3)
+  ),
+  drawSymbol('*', FontSize),
+  NextColumn is ColumnNumber + FontSize,
+  drawA(TextWidth, TextHeight, FontSize, CurrentLine, NextColumn).
+
+
+drawA(TextWidth, TextHeight, FontSize, CurrentLine, ColumnNumber) :-
+  (ColumnNumber >= FontSize, ColumnNumber < FontSize * 2),
+  (
+    (CurrentLine >= FontSize, CurrentLine < 2 * FontSize);
+    (CurrentLine >= FontSize * 3, CurrentLine < TextHeight)
+  ),
+  drawSymbol(' ', FontSize),
+  NextColumn is ColumnNumber + FontSize,
+  drawA(TextWidth, TextHeight, FontSize, CurrentLine, NextColumn).
+/* draw A */
+/*-------------------------------------------------------------------------------------------------*/
+
+
+
+/* draw the text with appropriate spacing*/
+draw(LeftRightMargin, SpaceBetweenCharacters, FontSize, CurrentLine, TextWidth, TextHeight) :-
+  CurrentLine >= TextHeight.
+draw(LeftRightMargin, SpaceBetweenCharacters, FontSize, CurrentLine, TextWidth, TextHeight) :-
+  CurrentLine < TextHeight,
+  ColumnNumber is 0,
+  write('|'), drawSymbol(' ', LeftRightMargin),
+  drawA(TextWidth, TextHeight, FontSize, CurrentLine, ColumnNumber),
+  /*---------------------------------------------*/
+  /** WRITE YOUR CODES HERE **/
+  % add spaces here between A and S
+  % drawS
+  % add spaces here between S and U
+  $ drawU
+  /*---------------------------------------------*/
+  drawSymbol(' ', LeftRightMargin),
+  write('|'),
+  nl,
+  NextLine is CurrentLine + 1,
+  draw(LeftRightMargin, SpaceBetweenCharacters, FontSize, NextLine, TextWidth, TextHeight).
+
+
+
+/* this will be initiating the variables and will be called from asu() */
+drawVerticalLinesWithCharacters(LeftRightMargin, BottomTopMargin, SpaceBetweenCharacters, FontSize) :-
+  CurrentLine is 0,
+  TextWidth is FontSize * 3,
+  TextHeight is FontSize * 5,
+  draw(LeftRightMargin, SpaceBetweenCharacters, FontSize, CurrentLine, TextWidth, TextHeight).
+
+
+
+/* this will be used from the console */
+asu(LeftRightMargin, BottomTopMargin, SpaceBetweenCharacters, FontSize) :-
+  /* verify that the variables are integers */
+  integer(LeftRightMargin), integer(BottomTopMargin), integer(SpaceBetweenCharacters), integer(FontSize),
+
+  /* calculate the height and width */
+  Width is (LeftRightMargin * 2 + SpaceBetweenCharacters * 2 + FontSize * 3 * 3 + 2),
+  Height is (BottomTopMargin * 2 + FontSize * 5),
+
+  /* top horizontal line of the box */
+  drawHorizontalLine('-', Width),
+  nl,
+
+  /* the empty space in the top */
+  drawVerticalLinesWithSpace('|', BottomTopMargin, Width),
+  nl,
+
+  /* the actual text */
+  drawVerticalLinesWithCharacters(LeftRightMargin, BottomTopMargin, SpaceBetweenCharacters, FontSize), 
+  nl,
+
+  /* the empty space in the bottom */
+  drawVerticalLinesWithSpace('|', BottomTopMargin, Width),
+  nl,
+
+  /* bottom horizontal line of the box */
+  drawHorizontalLine('-', Width).
