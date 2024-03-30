ee2195350ffdffd7a0ea90f131ab72aac50abc03
Waqar-107 <waqar.hassan866@gmail.com>
2024-03-30 15:26:49 -0700

    added recitation 10

--- Project-templates/Wangs Algorithm/wangs_algorithm.pl
+++ Project-templates/Wangs Algorithm/wangs_algorithm.pl
@@ -1,7 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%             ASU CSE 259 Logic in CS           %%
-%%             Project Submission File           %%
-%%                   thmpv.py                    %%
+%%                    Project 3                  %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -49,8 +48,98 @@ rmvDup([H | Tail], Tail1):-
 
 % Implement all other non-branching rules below by following Wangs algorithm
 
+/*
+ * Rule-1
+ * example rule: negation
+ * non-branching rule
+ * If one of the formulae separated by commas is the
+ * negation of a formula, drop the negation sign and
+ * move it to the other side of the arrow.
+ */
+prove(L => R):-
+    member(~X, L),
+    del(~X, L, NewL),
+    nl, write('=\t'), write(NewL => [X | R]),
+    write('\t (by negation/left)'),
+    prove(NewL => [X | R]).
+prove(L => R):-
+    member(~X, R),
+    del(~X, R, NewR),
+    nl, write('=\t'), write([X | L] => NewR),
+    write('\t (by negation/right)'),
+    prove([X | L] => NewR).
+
+
+/*
+ * Rule-2
+ * non-branching rule
+ * If the last connective of a formula on the left is ^ (and),
+ * or on the right of the arrow is v (or), replace the connective by a comma.
+ */
+
+% left conjuction
+prove(L => R) :-
+  member(A ^ B, L),
+  del(A ^ B, L, NewL),
+  nl, write('=\t'), write([A, B | NewL] => R),
+  write('\t (by and/left)'),
+  prove([A, B | NewL] => R).
+
+% right disjunction
+prove(L => R) :-
+  member(A v B, R),
+  del(A v B, R, NewR),
+  nl, write('=\t'), write(L => [A, B | NewR]),
+  write('\t (by or/right)'),
+  prove(L => [A, B | NewR]).
+
+
+/*
+ * Rule-3
+ * non-branching rule
+ * If the last connective of a formula on the right is A ➝ B, remove A ➝ B from the right 
+ * and then add A to the left and B to the right.
+ */
+% Write your code here
+
 
 % Implement all branching rules below by following Wangs algorithm
+/*
+ * Rule-4
+ * branching rule
+ * If the last connective of a formula on the left is v (or), or on the right of the arrow is ^ (and), 
+ * then produce two new lines, each with one of the two sub formulae replacing the formula. 
+ * Both of these must be proved in order to prove the original theorem.
+ */
+% Write your code here
+
+
+
+/*
+ * Rule-5
+ * example rule: left implication
+ * branching rule
+ * If the last connective of a formula on the left is A ➝ B,
+ * remove A ➝ B from the left and then create two new lines, 
+ * one with B added to the left, and the other with A added to the right.
+ */
+prove(L => R) :-
+  member(A -> B, L),
+  del(A -> B, L, NewL),
+  nl,
+  write('\tFirst branch: '),
+  nl,
+  write('=\t'),
+  write([B | NewL] => R),
+  write('\t (by arrow/left)'),
+  prove([B | NewL] => R),
+  nl,
+  write('\tSecond branch: '),
+  nl,
+  write('=\t'),
+  write(NewL => [A | R]),
+  write('\t (by arrow/left)'),
+  prove(NewL => [A | R]).
 
 %%%%%%%%%%%%%%%% YOUR CODE ENDS %%%%%%%%%%%%%%%%
 
